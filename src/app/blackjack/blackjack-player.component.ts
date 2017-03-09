8a5b5ca33468828abb96de5005f8f83d204bb466
Chris Cotar <christopher.cotar@blackbaud.com>
2017-03-09 15:46:09 -0600

    added some player spaces

---
+++ src/app/blackjack/blackjack-player.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'blackjack-player',
+  templateUrl: './blackjack-player.component.html'
+})
+export class BlackjackPlayerComponent {}
